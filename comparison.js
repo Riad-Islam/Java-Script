7118f33b6237376fd7019abcea5b8e625185890a
Riad-Islam <riadislam20471@gmail.com>
2026-05-04 12:00:01 +0600

    Comparison

---
+++ comparison.js
@@ -0,0 +1,20 @@
+// console.log(5 > 10);
+// console.log(5 == 5);
+// console.log(34 >= 22);
+// console.log(5 >= 10);
+// console.log(6 <= 7);
+
+// var applePrice = 300;
+// var PeyaraPrice = 50;
+// console.log(applePrice < PeyaraPrice);
+
+var num1 = 10;
+var num2 = '10';
+// console.log(num1 == num2); 
+// //Compare the numbers only
+
+
+// console.log(num1 === num2);
+// Compare data type 
+
+console.log(10 != 10);
