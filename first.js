f91962c0f7f6a05d02b7d83f45d113e1219bf51d
Riad-Islam <riadislam20471@gmail.com>
2026-05-04 10:26:17 +0600

    Practice JavaScript Variables

---
+++ first.js
@@ -0,0 +1,15 @@
+// console.log(66)
+
+// Variable
+
+// var numbers = 44;
+// console.log(numbers);
+
+var name = "Riad Islam";
+var address = "Sharat Gupta Road";
+var isreach = true;  //Boolean type data
+console.log(name);
+console.log(address);
+console.log(isreach);
+console.log(typeof isreach);
+
