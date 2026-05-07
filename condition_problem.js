b8a1274804967f96a5e06a8fca260c3787dc0897
Riad-Islam <riadislam20471@gmail.com>
2026-05-07 10:13:01 +0600

    add some conditional problems

---
+++ condition_problem.js
@@ -0,0 +1,41 @@
+/**
+ * BMI Problem
+ */
+
+// const weight = 80;
+// var height = 120;
+// var BMI = weight / (height)^2;
+
+// if (BMI < 18.5){
+//     console.log('You are underweight');
+// }else if(BMI >= 18.5 && BMI <= 24.9){
+//     console.log("You are normal");
+// }else if(BMI >= 25 && BMI <=29.9){
+//     console.log("you are overweight");
+// }else{
+//     console.log("You are obese");
+// }
+
+
+
+
+/**
+ * Good Luck Friend
+ */
+
+var score = 80;
+var fScore = 89;
+
+if (score > 80){
+    if(fScore > 80){
+        console.log("Both can go for a lunch");
+    }else if(fScore >= 60 && fScore < 80){
+        console.log("Tell your friend good luck");
+    }else if(fScore >= 40 && Fscore < 60){
+        console.log("Unseen Your Friend Message");
+    }else{
+        console.log("Block your friend");
+    }
+}else{
+    console.log("Go to home and sleep");
+}
