7da7f70d1edc4f1ae9a72c65d5b2266ce4d1bb3b
Riad-Islam <riadislam20471@gmail.com>
2026-05-10 11:34:23 +0600

    Add for loop problem

--- for_loop.js
+++ for_loop.js
@@ -8,10 +8,32 @@ for (let i=1; i<len; i++){
 }
 */
 
+/** 
 const names = ["akul", "bakul", "mokul"];
 let len = names.length;
 
 for(let i=0; i<len; i++){
     console.log(names[i]);
 }
+*/
 
+// sum of number between 11 to 20
+/** let sum = 0;
+for (let i=11; i<=20; i++){
+    sum = sum+i;
+}
+console.log(sum); */  
+
+
+// for (let i=1; i<=30; i++){
+//     if(i%3 == 0 || i%5 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+for (let i=1; i<=30; i++){
+    if(i%3 == 0 && i%5 == 0){
+        console.log(i);
+    }
+}
