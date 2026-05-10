9998c6ee8bfce8b5c6239ce0e828e2d3af2cff3b
Riad-Islam <riadislam20471@gmail.com>
2026-05-10 09:53:29 +0600

    While Loop

---
+++ for_loop.js
@@ -0,0 +1,17 @@
+
+/** 
+const numbers = [1, 2, 3, 4, 5, 6];
+let len = numbers.length;
+
+for (let i=1; i<len; i++){
+    console.log(numbers[i]);
+}
+*/
+
+const names = ["akul", "bakul", "mokul"];
+let len = names.length;
+
+for(let i=0; i<len; i++){
+    console.log(names[i]);
+}
+
---
+++ while.js
@@ -0,0 +1,44 @@
+// let num = 0;
+
+// while (num < 5){
+//     console.log("looping", num);
+//     // num = num+1;
+//     num++;
+// }
+
+// Sum numbers from 1 to 10
+
+// let number = 1;
+// let sum = 0;
+/**
+while(number <= 10){
+    sum = sum + number;
+    number ++;
+}
+console.log('Total : ', sum);
+*/
+
+
+// Show only even numbers 
+/** 
+let num = 1;
+
+while (num <= 10){
+    if((num % 2) == 0){
+        console.log(num);
+        num++;
+    }else{
+        num++;
+    }
+}
+*/
+
+
+// Multiplication problem of 5
+let num = 1;
+
+while(num <= 10){
+    let multi = num * 5;
+    console.log('5 X ',num,':',multi);
+    num++;
+}
